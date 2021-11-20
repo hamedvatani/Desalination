e469780ab72a2241f8d00b97a29eecc29b0a94de
Hamed Vatani <hamedv@gmail.com>
2021-11-20 12:29:12 +0330

    Add splash form

---
+++ Solution/Desalination/Desalination/frmSplash.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desalination
+{
+    public partial class frmSplash : Form
+    {
+        public frmSplash()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmSplash_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape || e.KeyCode == Keys.Enter || e.KeyCode == Keys.Space)
+                Close();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
